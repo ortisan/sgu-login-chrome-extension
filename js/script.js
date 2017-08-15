5e72ef2e5fb3fd9d3d6c7e2516fc644006175f00
Marcelo Ortiz de Santana <mosantana@apoioprodesp.sp.gov.br>
2017-08-15 13:12:21 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+(function (jQuery) {
+    jQuery("[name='form-login:input:usuario']").val("xpto");
+    jQuery("[name='form-login:input:senha']").val("xpto");
+    jQuery("button").click();
+})(jQuery);
